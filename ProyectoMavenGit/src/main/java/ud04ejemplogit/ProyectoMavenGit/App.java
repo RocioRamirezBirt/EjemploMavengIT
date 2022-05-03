e1ebdb59e1670c767cd5fcd4e05a5e6fd2ee2e3d
saravelvet01 <100027791+saravelvet01@users.noreply.github.com>
2022-05-03 23:58:45 +0200

    Update App.java
    descripcion  hecha desde github

--- ProyectoMavenGit/src/main/java/ud04ejemplogit/ProyectoMavenGit/App.java
+++ ProyectoMavenGit/src/main/java/ud04ejemplogit/ProyectoMavenGit/App.java
@@ -11,5 +11,7 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("este es mi primer commit");
         System.out.println("este es mi segundo commit");
+        System.out.println("este es mi tercer commit");
+        System.out.printn("Ejemplo desde mi Github");
     }
 }
